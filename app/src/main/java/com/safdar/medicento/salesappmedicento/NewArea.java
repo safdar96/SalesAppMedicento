8d13909167a1e6d39bfd50976761a5c5cdce8d8c
GiteshShastri <35490411+GiteshShastri@users.noreply.github.com>
2018-06-09 17:57:46 +0530

    Added Area And Pharmacy Layout

---
+++ app/src/main/java/com/safdar/medicento/salesappmedicento/NewArea.java
@@ -0,0 +1,13 @@
+package com.safdar.medicento.salesappmedicento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_area);
+    }
+}
