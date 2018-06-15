4c03173aee163816cfa8b973fda90a53dcce8cda
Syed Mohammad Safdar Ali <safdarontheweb@gmail.com>
2018-06-15 11:45:50 +0530

    Revert "Added Changes To screenn 2"

--- app/src/main/java/com/safdar/medicento/salesappmedicento/Details.java
+++ app/src/main/java/com/safdar/medicento/salesappmedicento/Details.java
@@ -3,7 +3,6 @@ package com.safdar.medicento.salesappmedicento;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 public class Details extends AppCompatActivity {
@@ -16,9 +15,5 @@ public class Details extends AppCompatActivity {
         Intent intent = getIntent();
         String texte = intent.getStringExtra("text");
         text.setText(texte);
-    }
-    public void goBack(View view) {
-        Intent intent = new Intent(Details.this, MainActivity.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/safdar/medicento/salesappmedicento/SalesPersonDetails.java
+++ app/src/main/java/com/safdar/medicento/salesappmedicento/SalesPersonDetails.java
@@ -1,9 +1,7 @@
 package com.safdar.medicento.salesappmedicento;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class SalesPersonDetails extends AppCompatActivity {
 
@@ -12,13 +10,4 @@ public class SalesPersonDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_person_details);
     }
-    public void signOut(View view) {
-        Intent intent = new Intent(SalesPersonDetails.this,MainActivity.class);
-        startActivity(intent);
-    }
-    public void goBack(View view) {
-        Intent intent = new Intent(SalesPersonDetails.this,MainActivity.class);
-        startActivity(intent);
-    }
 }
-
